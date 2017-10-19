6b98598473b26d127fcb0c5eeeb00086f7bf87b8
c-andres <charlesalexanderandres@gmail.com>
2017-10-19 12:04:23 -0700

    new iteration with no distance matrix

--- assets/js/walk-run.js
+++ assets/js/walk-run.js
@@ -1,225 +1,148 @@
-  // Note: This example requires that you consent to location sharing when
-      // prompted by your browser. If you see the error "The Geolocation service
-      // failed.", it means you probably did not give permission for the browser to
-      // locate you.
-      var map, infoWindow;
-      var marker;
-      var path = "";
-      var coordArray = [];
-      var trackBol;
-      var interval;
-      var coordMinusArray = [];
-      var outputDiv = document.getElementById('output');
-      var distance;
-      var totalDistance;
+var map, infoWindow;
+var marker;
+var path = "";
+var coordArray = [];
+var trackBol;
+var interval;
+var outputDiv = document.getElementById('output');
+var distance;
+var totalDistance;
+var lat1 = 0;
+var lng1 = 0;
+var lat2 = 0;
+var lng2 = 0;
 
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat:34.052235, lng:-118.243683},
-          zoom: 9
-        });
-        infoWindow = new google.maps.InfoWindow;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: 34.052235, lng: -118.243683 },
+		zoom: 9
+	});
+	infoWindow = new google.maps.InfoWindow;
 
-        function setMarkerPosition(marker, position) {
-          marker.setPosition(
-              new google.maps.LatLng(
-                  position.coords.latitude,
-                  position.coords.longitude)
-          );
-        };
-        function showLocation(position) {
-            var i = 0;
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            var coords = {lat:lat, lng:lng};
-            coordArray.push(coords);
+	function setMarkerPosition(marker, position) {
+		marker.setPosition(
+			new google.maps.LatLng(
+				position.coords.latitude,
+				position.coords.longitude)
+		);
+	};
+	/*computes current location, pushes the coordinates to an array for future drawing use, 
+	stores the newest coordinates in initial variable, when next coordinates are calculated stores previous coordinates in lat2 lng2,
+	stores newest coordinates in lat1 lng1.
+	*/
+	function showLocation(position) {
+		var lat = position.coords.latitude;
+		var lng = position.coords.longitude;
+		setMarkerPosition(marker, position);
+		storeNewCoord(lat, lng);
+		if (lat1 === 0){
+			lat1 = lat;
+			lng1 = lng;
+		}
+		else {
+			lat2 = lat1;
+			lng2 = lng1;
+			lat1 = lat;
+			lng2 = lat
+			getDistanceFromLatLon(lat1, lng1, lat2, lng2);
+		}
 
-;
+	};
+	//function that will allow us to draw lines with previous coordinates
+	function storeNewCoord(lat, lng) {
+		var coord = "new google.maps.LatLng(" + lat.toString() + ", " + lng.toString() + ")";
+		coordArray.push(coord);
+	};
+	//function that calculates distance using great circle method (arc instead of flat line)
+	function getDistanceFromLatLon(lat1,lon1,lat2,lon2) {
+	  var R = 3959; // Radius of the earth in miles
+	  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	  var dLon = deg2rad(lon2-lon1); 
+	  var a = 
+	    Math.sin(dLat/2) * Math.sin(dLat/2) +
+	    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	    Math.sin(dLon/2) * Math.sin(dLon/2)
+	    ; 
+	  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	  var d = R * c; // Distance in miles
+	  totalDistance += d;
+	  $("#distance").html(totalDistance);
+	};
+	//function that converts degs to radians
+	function deg2rad(deg) {
+	  return deg * (Math.PI/180)
+	};
 
+	function errorHandler(err) {
+		if (err.code == 1) {
+			alert("Error: Access is denied!");
+		} else if (err.code == 2) {
+			alert("Error: Position is unavailable!");
+		}
+	};
 
-            // alert("Latitude : " + latitude + " Longitude: " + longitude);
-            setMarkerPosition(marker,position);
-            console.log(lat);
-            console.log(lng);
-            console.log(coordArray);
+	$("#start").on("click", function(e) {
+		e.preventDefault();
+		var sec1 = 0;
+		var sec2 = 0;
+		var min = 0;
+		var timer = setInterval(function(){
+			sec1++;
+			if (sec1 < 10) {
+				$(".timer").html(`0${min}:${sec2}${sec1}`);
+			}
+			else if (sec1 === 10) {
+				sec2++;
+				sec1 = 0;
+				if (sec2 < 6) {$(".timer").html(`0${min}:${sec2}${sec1}`)}
+				else if (sec2 >= 6) {
+					sec2 = 0;
+					min++;
+					$(".timer").html(`0${min}:${sec2}${sec1}`)
+				}
+			}
+		}, 1000);
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				var pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+				map.setCenter(pos);
+				map.setZoom(14);
+				var options = {
+					enableHighAccuracy: true,
+					timeout: Infinity,
+					maximumAge: 1000
+				};
+				marker = new google.maps.Marker({
+					position: pos,
+					map: map,
+					icon: "./assets/images/paw.png"
+				});
+				watchID = navigator.geolocation.watchPosition(showLocation, errorHandler, options);
+			}, function() {
+				handleLocationError(true, infoWindow, map.getCenter());
+			});
+		} else {
+			// Browser doesn't support Geolocation
+			handleLocationError(true, infoWindow, map.getCenter());
+		}
+	});
 
-            $("#output").html(coords) ;
-            i++;
-            if(coordArray.length > 1){
-              distanceCalc();
-            }
-         };
-         //for each set of coordinates, place them in path 
-            //when a coordinate set is added, place a "|" before it
-        // function makePath(){
-
-        //   for (var i = 0; i < coordArray.length; i++) {
-        //     if ( coordArray === coordArray)
-        //       path += coordArray[i];
-        //     else if ( i === 1 ){
-        //       path += coordArray[i];
-        //       console.log(path);
-        //     }
-        //     // else if (i < 25)
-        //     // else if (i < 50)
-
-        //     else{
-        //       path += "\|"+coordArray[i], coordArray[i+1];
-        //       console.log(path);
-        //     }
-
-        //   }
-        // }
-         
-         function errorHandler(err) {
-            if(err.code == 1) {
-               alert("Error: Access is denied!");
-            }
-            
-            else if( err.code == 2) {
-               alert("Error: Position is unavailable!");
-            }
-         };
-
-        $("#start").on("click", function(e){
-          e.preventDefault();
-          // interval = window.setInterval(makePath, 10000);
-          if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };  
-            map.setCenter(pos);
-            map.setZoom(14);
-            var options = {
-              enableHighAccuracy: true,
-              timeout:Infinity,
-              maximumAge:0
-            };
-            marker = new google.maps.Marker({
-              position: pos,
-              map: map,
-              icon: "./assets/images/paw.png"
-            });
-            watchID = navigator.geolocation.watchPosition(showLocation, errorHandler, options);
-              }, function() {
-              handleLocationError(true, infoWindow, map.getCenter()); 
-            });
-        }            else {
-          // Browser doesn't support Geolocation
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-        });
-        // Try HTML5 geolocation.
-        // $("#stop").on(click, function(e){
-        //   e.preventDefault();
-        //   startBol = false;
-
-        //   } 
-
-
-
-
-      $("#stop").on("click", function(e){
-        e.preventDefault();
-        trackBol = true;
-        
-        // clearInterval(interval);
-
-      });
-      
-      // var bounds = new google.maps.LatLngBounds;
-      //   var markersArray = [];
-
-
-        // var destinationIcon = 'https://chart.googleapis.com/chart?' +
-        //     'chst=d_map_pin_letter&chld=D|FF0000|000000';
-        // var originIcon = 'https://chart.googleapis.com/chart?' +
-        //     'chst=d_map_pin_letter&chld=O|FFFF00|000000';
-        // // var map = new google.maps.Map(document.getElementById('map'), {
-        // //   center: {lat: 55.53, lng: 9.4},
-        // //   zoom: 10
-        // // });
-        var geocoder = new google.maps.Geocoder;
-        
-
-        function distanceCalc(){
+	$("#stop").on("click", function(e) {
+		e.preventDefault();
+		trackBol = true;
+	});
 
 
 
-          var i = 1;
-          // var distance = results[j].distance.text;
-          var service = new google.maps.DistanceMatrixService;
-          service.getDistanceMatrix({
-            origins: [coordArray[i-1]],
-            destinations: [coordArray[i]],
-            travelMode: 'WALKING',
-            unitSystem: google.maps.UnitSystem.IMPERIAL,
-            avoidHighways: true,
-            avoidTolls: true,
-        }, function (response, status) {
-          if (status !== 'OK') {
-            alert('Error was: ' + status);
-          } else{
-            var originList = response.originAddresses;
-            var destinationList = response.destinationAddresses;
-            outputDiv = document.getElementById('output');
-            outputDiv.innerHTML = '';
-            // deleteMarkers(markersArray);
+};
 
-            // var showGeocodedAddressOnMap = function(asDestination) {
-            //   var icon = asDestination ? destinationIcon : originIcon;
-            //   return function(results, status) {
-            //     if (status === 'OK') {
-            //       map.fitBounds(bounds.extend(results[0].geometry.location));
-            //       markersArray.push(new google.maps.Marker({
-            //         map: map,
-            //         position: results[0].geometry.location,
-            //         icon: icon
-            //       }));
-            //     } else {
-            //       alert('Geocode was not successful due to: ' + status);
-            //     }
-            //   };
-            // };
-
-            for (var i = 0; i < originList.length; i++) {
-              var results = response.rows[i].elements;
-              geocoder.geocode({'address': originList[i]},
-);
-              for (var j = 0; j < results.length; j++) {
-                geocoder.geocode({'address': destinationList[j]},);
-                  distance = results[j].distance 
-                  console.log(distance.value);
-              $("#output").html("Your Distance Traveled:" + distance.text); 
-
-                if (trackBol === true)
-                   $("#output").html("Your Distance Traveled:" + distance);
-
-              };
-              console.log( typeof distance);
-              console.log(distance);
-              distance += distance 
-
-            
-            }
-
-          }
-          console.log(distance);
-        });
-      i++;
-    };
-  };
-
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  };
-
-
-
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+		'Error: The Geolocation service failed.' :
+		'Error: Your browser doesn\'t support geolocation.');
+	infoWindow.open(map);
+};
